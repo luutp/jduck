3f9fab95fa6d914153b0ba69a263cb15e259a8fb
trieuphatluu <tpluu2207@gmail.com>
2021-02-27 00:53:23 -0600

    add widget control

---
+++ jduck/DCMotor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+DCMotor.py
+Description:
+
+Author: luutp
+Contact: luubot2207@gmail.com
+Created on: 2021/02/27
+'''
+
+#%%
+# ================================IMPORT PACKAGES====================================
